9bd4ecde9249082ae0f65e75be85af42ef4b8276
Thaetami <tatanghaetami.97@gmail.com>
2023-04-19 15:48:57 +0700

    update validation song service

--- src/validator/songs/schema.js
+++ src/validator/songs/schema.js
@@ -1,10 +1,10 @@
 const Joi = require('joi')
 
 const SongPayloadSchema = Joi.object({
-  title: Joi.string().required(),
-  year: Joi.number().required(),
-  genre: Joi.string().required(),
-  performer: Joi.string().required(),
+  title: Joi.string().required().min(3).trim(),
+  year: Joi.number().required().integer().min(1000).max(9999),
+  genre: Joi.string().required().min(3).trim(),
+  performer: Joi.string().required().min(3).trim(),
   duration: Joi.number(),
   albumId: Joi.string()
 })
